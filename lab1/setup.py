c7b1a44d7a6f6a232631609f003bdfcf9fec5c53
Rev-ex <kacperm102@gmail.com>
2021-03-08 13:14:55 +0100

    lab1

---
+++ lab1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'lab1'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kacper',
+    maintainer_email='kacperm102@gmail.com',
+    description='Lab1 - zadanie',
+    license='licencja',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'node = lab1.node:main'
+        ],
+    },
+)
