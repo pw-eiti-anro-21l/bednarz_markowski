9e6c044d051c0ce811e086e9637189fda7c7cd55
Rev-ex <kacperm102@gmail.com>
2021-05-09 19:26:46 +0200

    bbbbalbinka

--- lab4/lab4/jint.py
+++ lab4/lab4/jint.py
@@ -1,5 +1,7 @@
 import sys
 import time
+import math
+import threading
 from lab4_srv.srv import Jint
 import rclpy
 from rclpy.qos import QoSProfile
@@ -9,7 +11,10 @@ from geometry_msgs.msg import Quaternion, PoseStamped
 from sensor_msgs.msg import JointState
 from tf2_ros import TransformBroadcaster, TransformStamped
 
-
+class Limit: #prosta klasa do przechowania limitow jointow
+    def __init__(self, lower, upper):
+        self.lower = lower
+        self.upper = upper
 
 class JintService(Node):
 
@@ -34,45 +39,88 @@ class JintService(Node):
         self.j1 = 0.0
         self.j2 = 0.0
         self.j3 = 0.0
+        self.currentJ1 = self.j1
+        self.currentJ2 = self.j2
+        self.currentJ3 = self.j3
+
+        self.j1Limit = Limit(-3.14, 3.14)
+        self.j2Limit = Limit(-1.57, 0.0)
+        self.j3Limit = Limit(-1.57, 1.57)
+
+        self.result=""
+
+        publishingThread = threading.Thread(target=self.publishNewStates)
+        publishingThread.start()
         
     def jint_control_srv_callback(self, request, response):
+
+        if request.time <= 0:
+            response.output = "Invalid interpolation time; Interpolation impossible; Terminating"
+            return response
+
+        if request.type != "Linear" and request.type != "Spline":
+            response.output = "Invalid interpolation type; Avaible types: Linear, Spline; Terminating"
+            return response
+
+        
+        self.currentJ1 = self.j1
+        self.currentJ2 = self.j2
+        self.currentJ3 = self.j3
+        targetJ1 = request.j1pose
+        targetJ2 = request.j2pose
+        targetJ3 = request.j3pose
+        targetTime = request.time
+    	
         T = 0.1
         steps = int(request.time/T)
-        currentJ1 = self.j1
-        currentJ2 = self.j2
-        currentJ3 = self.j3
-    	
-        if request.type == "Linear":
-            for k in range(1, steps):
-                now = self.get_clock().now()
-                currentJ1 = self.j1 + (request.j1pose-self.j1)/(request.time) *k*T
-                currentJ2 = self.j2 + (request.j2pose-self.j2)/(request.time) *k*T
-                currentJ3 = self.j3 + (request.j3pose-self.j3)/(request.time) *k*T
-                #currentJ1 = self.interpolateLin(self.j1, request.j1pose, steps, k)
-                #currentJ2 = self.interpolateLin(self.j2, request.j2pose, steps, k)
-                #currentJ3 = self.interpolateLin(self.j3, request.j3pose, steps, k)
-    			
+        for k in range(1, steps+1):
+            if request.type == "Linear":
+                self.currentJ1 = self.interpolateLin(self.j1, targetJ1, targetTime, k*T)
+                self.currentJ2 = self.interpolateLin(self.j2, targetJ2, targetTime, k*T)
+                self.currentJ3 = self.interpolateLin(self.j3, targetJ3, targetTime, k*T)
 
+            if request.type == "Spline":
+                self.currentJ1 = self.interpolateSpline(self.j1, targetJ1, targetTime, k*T)
+                self.currentJ2 = self.interpolateSpline(self.j2, targetJ2, targetTime, k*T)
+                self.currentJ3 = self.interpolateSpline(self.j3, targetJ3, targetTime, k*T)
+            time.sleep(0.1)
+
+        self.j1 = self.currentJ1
+        self.j2 = self.currentJ2
+        self.j3 = self.currentJ3
+        response.output= "Interpolation ( " + request.type + " ) succesful! Absolute errors: " + \
+        str(abs(targetJ1 - self.j1)) + ", " + str(abs(targetJ2 - self.j2)) + ", " + str(abs(targetJ3 - self.j3))
+        return response
+
+    def publishNewStates(self):
+        while True:
+            try:
                 now = self.get_clock().now()
                 self.joint_state.header.stamp = now.to_msg()
                 self.joint_state.name = self.names
-                self.joint_state.position = [currentJ1, currentJ2, currentJ3]
-                  # update transform
-                 # (moving in a circle with radius=2)
+                self.joint_state.position = [self.currentJ1, self.currentJ2, self.currentJ3]
+                # update transform
+                # (moving in a circle with radius=2)
                 self.odom_trans.header.stamp = now.to_msg()
-
-                 # send the joint state and transform
+                # send the joint state and transform
                 self.publisher.publish(self.joint_state)
                 self.broadcaster.sendTransform(self.odom_trans)
-                time.sleep(T)
-            self.j1 = currentJ1
-            self.j2 = currentJ2
-            self.j3 = currentJ3
-        return response
+                time.sleep(0.1)
+            except KeyboardInterrupt:
+                exit(0)
 
-    def interpolateLin(x0, x1, step, t): #t - chwila czasu
-        return x0 + (x1-x0)/step*t
+    def interpolateLin(self, x0, x1, t, timePassed): 
+        return x0 + (x1-x0)/t*timePassed
 
+    def interpolateSpline(self, x0, x1, t, timePassed):
+        #wzor z wikipedii, t0 = 0
+        tx = timePassed/t
+        k1 = 0 
+        k2 = 0 # pochodne sa zerowe
+        a = k1*t - (x1-x0)
+        b = -k2*t + (x1-x0)
+        qx = (1-tx)*x0 + tx*x1 + tx*(1-tx)*((1-tx)*a+tx*b) 
+        return qx
 
 
 def main(args=None):
