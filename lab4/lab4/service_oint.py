0482cfb62bd4638db6d3d78a0d15c116eb95363e
Your Name <you@example.com>
2021-05-09 22:08:30 +0200

    dodanie oint

--- lab4/lab4/service_oint.py
+++ lab4/lab4/service_oint.py
@@ -6,6 +6,8 @@ from rclpy.qos import QoSProfile
 from rclpy.node import Node
 from rclpy.clock import ROSClock
 from geometry_msgs.msg import Quaternion, PoseStamped
+from math import sin, cos
+import threading
 
 
 class OintService(Node):
@@ -15,36 +17,66 @@ class OintService(Node):
         self.srv = self.create_service(Oint, 'oint_control_srv', self.oint_control_srv_callback)
         self.publisher = self.create_publisher(PoseStamped, "/pose", QoSProfile(depth=10))
         self.pose_stamped = PoseStamped()
-        self.pose_stamped.header.frame_id = "baza"
+        self.pose_stamped.header.frame_id = "odom"
         #pozycja startowa
-        self.x = 0
-        self.y = 0
-        self.z = 0
-        self.roll = 0
-        self.pitch = 0
-        self.yaw = 0
+        self.x = 0.0
+        self.y = 0.0
+        self.z = 0.0
+        self.roll = 0.0
+        self.pitch = 0.0
+        self.yaw = 0.0
+        
+        publishingThread = threading.Thread(target=self.publishNewStates)
+        publishingThread.start()
         
 
     def oint_control_srv_callback(self, request, response):
     	T = 0.1
-    	steps = int(request.time/T)
     	
     	if request.type == "Linear":
-    		for k in range(1, steps):
+    		steps = int(request.time/T)
+    		delta_x = (request.x-self.x)/steps
+    		delta_y = (request.y-self.y)/steps
+    		delta_z = (request.z-self.z)/steps
+    		delta_roll = (request.roll-self.roll)/steps
+    		delta_pitch = (request.pitch-self.pitch)/steps
+    		delta_yaw = (request.yaw-self.yaw)/steps
+    	
+    		for k in range(1, steps+1):
     			now = self.get_clock().now()
-    			self.pose_stamped.position.x = self.x + (request.x-self.x)/steps*k
-    			self.pose_stamped.position.y = self.y + (request.y-self.y)/steps*k
-    			self.pose_stamped.position.z = self.z + (request.x-self.z)/steps*k
+    			self.pose_stamped.header.stamp = now.to_msg()
+    			self.x += delta_x
+    			self.pose_stamped.pose.position.x = self.x
+    			self.y += delta_y
+    			self.pose_stamped.pose.position.y = self.y
+    			self.z += delta_z
+    			self.pose_stamped.pose.position.z = self.z
+    			self.roll += delta_roll
+    			self.pitch += delta_pitch
+    			self.yaw += delta_yaw
     			
-    			current_roll = self.roll +(request.roll-self.roll)/steps*k
-    			current_pitch = self.pitch +(request.pitch-self.pitch)/steps*k
-    			current_yaw = self.yaw +(request.yaw-self.yaw)/steps*k
-    			self.pose_stamped.pose.orientation = self.euler_to_quaternion(current_roll, current_pitch, current_yaw) 
+    			self.pose_stamped.pose.orientation = self.euler_to_quaternion(self.roll, self.pitch, self.yaw) 
     			self.publisher.publish(self.pose_stamped)
     			time.sleep(T)
-    	return response
-   
-    def euler_to_quaternion(roll, pitch, yaw):
+
+    		return response
+    		
+    def publishNewStates(self):
+    	while True:
+    		try:
+	    		now = self.get_clock().now()
+	    		self.pose_stamped.pose.position.x = self.x
+	    		self.pose_stamped.pose.position.y = self.y
+	    		self.pose_stamped.pose.position.z = self.z
+	    		self.pose_stamped.pose.orientation = self.euler_to_quaternion(self.roll, self.pitch, self.yaw) 
+	    		self.pose_stamped.header.stamp = now.to_msg()
+	    		self.pose_stamped.header.frame_id = 'odom'
+	    		self.publisher.publish(self.pose_stamped)
+	    		time.sleep(0.1)
+	    	except KeyboardInterrupt:
+	    		exit(0)
+			
+    def euler_to_quaternion(self, roll, pitch, yaw):
     	qx = sin(roll/2) * cos(pitch/2) * cos(yaw/2) - cos(roll/2) * sin(pitch/2) * sin(yaw/2)
     	qy = cos(roll/2) * sin(pitch/2) * cos(yaw/2) + sin(roll/2) * cos(pitch/2) * sin(yaw/2)
     	qz = cos(roll/2) * cos(pitch/2) * sin(yaw/2) - sin(roll/2) * sin(pitch/2) * cos(yaw/2)
